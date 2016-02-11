52f8cf940ee16d587fbe4dd5efb74786fba44b29
Mieszko4 <mieszko4@gmail.com>
2016-02-11 12:50:17 +0100

    Inital commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+/*header("HTTP/1.1 503 Service Unavailable");
+header("Status: 503 Service Unavailable");
+header("Retry-After: ".(60*60*24));*/
+
+header("Location: http://rotodieselgelencir.hr/naslovna");
